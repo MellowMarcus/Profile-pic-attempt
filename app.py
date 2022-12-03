ddfe39571f760324f84d5471b10d74a8d614a347
Mellow is fine <114035155+MellowMarcus@users.noreply.github.com>
2022-12-03 12:12:47 -0400

    Update app.py

--- app.py
+++ app.py
@@ -7,34 +7,32 @@ from datetime import date
 from webforms import LoginForm, PostForm, UserForm, PasswordForm, NamerForm, SearchForm
 from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
 from webforms import LoginForm, PostForm, UserForm, PasswordForm, NamerForm
-from flask_ckeditor import CKEditor
 from werkzeug.utils import secure_filename
 import uuid as uuid
 import os
 
-
 # Create a Flask Instance
 app = Flask(__name__)
-# Add CKEditor
-ckeditor = CKEditor(app)
+
 # Add Database
-# Old SQLite DB
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://fvmdxyjigwbmiu:d31d36c7613e08f7ad37a6e40007189d7f2f44fbc99234b339994780a122d870@ec2-3-214-190-189.compute-1.amazonaws.com:5432/d17b5scj97ckqe'
-# New MySQL DB
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://username:password@localhost/db_name'
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:password123@localhost/our_users'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+
 # Secret Key!
 app.config['SECRET_KEY'] = "my super secret key that no one is supposed to know"
+
 # Initialize The Database
-
-UPLOAD_FOLDER = 'static/images/'
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-# Flask_Login Stuff
+#Pictures
+UPLOAD_FOLDER = 'static/images/'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+#VIEWS
+
+
+# Flask_Login
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
@@ -49,7 +47,7 @@ def base():
 	form = SearchForm()
 	return dict(form=form)
 
-# Create Admin Page
+#Admin
 @app.route('/admin')
 @login_required
 def admin():
@@ -62,30 +60,31 @@ def admin():
 
 
 
-# Create Search Function
+#Search 
 @app.route('/search', methods=["POST"])
 def search():
 	form = SearchForm()
-	posts = Posts.query
+	image = Imagess.query
 	if form.validate_on_submit():
 		# Get data from submitted form
-		post.searched = form.searched.data
+		image.searched = form.searched.data
 		# Query the Database
-		posts = posts.filter(Posts.content.like('%' + post.searched + '%'))
-		posts = posts.order_by(Posts.title).all()
+		image = image.filter(Images.content.like('%' + image.searched + '%'))
+		image = image.order_by(Images.title).all()
 
 		return render_template("search.html",
 		 form=form,
-		 searched = post.searched,
-		 posts = posts)
-# Create Login Page
+		 searched = image.searched,
+		 image = umage)
+
+
+#Login
 @app.route('/login', methods=['GET', 'POST'])
 def login():
 	form = LoginForm()
 	if form.validate_on_submit():
 		user = Users.query.filter_by(username=form.username.data).first()
 		if user:
-			# Check the hash
 			if check_password_hash(user.password_hash, form.password.data):
 				login_user(user)
 				flash("Login Succesfull!!")
@@ -98,15 +97,16 @@ def login():
 
 	return render_template('login.html', form=form)
 
-# Create Logout Page
+#Logout
 @app.route('/logout', methods=['GET', 'POST'])
 @login_required
 def logout():
 	logout_user()
-	flash("You Have Been Logged Out!  Thanks For Stopping By...")
+	flash("You Have Been Logged Out...")
 	return redirect(url_for('login'))
 
-# Create Dashboard Page
+
+#User Dashboard
 @app.route('/dashboard', methods=['GET', 'POST'])
 @login_required
 def dashboard():
@@ -114,25 +114,14 @@ def dashboard():
 	id = current_user.id
 	name_to_update = Users.query.get_or_404(id)
 	if request.method == "POST":
-		name_to_update.name = request.form['name']
-		name_to_update.email = request.form['email']
-		name_to_update.favorite_color = request.form['favorite_color']
 		name_to_update.username = request.form['username']
-		name_to_update.about_author = request.form['about_author']
-		
-
-		# Check for profile pic
+		#Checking profile pic
 		if request.files['profile_pic']:
 			name_to_update.profile_pic = request.files['profile_pic']
-
-			# Grab Image Name
 			pic_filename = secure_filename(name_to_update.profile_pic.filename)
-			# Set UUID
 			pic_name = str(uuid.uuid1()) + "_" + pic_filename
-			# Save That Image
 			saver = request.files['profile_pic']
 			
-
 			# Change it to a string to save to db
 			name_to_update.profile_pic = pic_name
 			try:
@@ -143,7 +132,7 @@ def dashboard():
 					form=form,
 					name_to_update = name_to_update)
 			except:
-				flash("Error!  Looks like there was a problem...try again!")
+				flash("Error! ...try again!")
 				return render_template("dashboard.html", 
 					form=form,
 					name_to_update = name_to_update)
@@ -161,121 +150,6 @@ def dashboard():
 
 	return render_template('dashboard.html')
 
-
-
-
-
-
-@app.route('/posts/delete/<int:id>')
-@login_required
-def delete_post(id):
-	post_to_delete = Posts.query.get_or_404(id)
-	id = current_user.id
-	if id == post_to_delete.poster.id or id == 14:
-		try:
-			db.session.delete(post_to_delete)
-			db.session.commit()
-
-			# Return a message
-			flash("Blog Post Was Deleted!")
-
-			# Grab all the posts from the database
-			posts = Posts.query.order_by(Posts.date_posted)
-			return render_template("posts.html", posts=posts)
-
-
-		except:
-			# Return an error message
-			flash("Whoops! There was a problem deleting post, try again...")
-
-			# Grab all the posts from the database
-			posts = Posts.query.order_by(Posts.date_posted)
-			return render_template("posts.html", posts=posts)
-	else:
-		# Return a message
-		flash("You Aren't Authorized To Delete That Post!")
-
-		# Grab all the posts from the database
-		posts = Posts.query.order_by(Posts.date_posted)
-		return render_template("posts.html", posts=posts)
-
-@app.route('/posts')
-def posts():
-	# Grab all the posts from the database
-	posts = Posts.query.order_by(Posts.date_posted)
-	return render_template("posts.html", posts=posts)
-
-@app.route('/posts/<int:id>')
-def post(id):
-	post = Posts.query.get_or_404(id)
-	return render_template('post.html', post=post)
-
-@app.route('/posts/edit/<int:id>', methods=['GET', 'POST'])
-@login_required
-def edit_post(id):
-	post = Posts.query.get_or_404(id)
-	form = PostForm()
-	if form.validate_on_submit():
-		post.title = form.title.data
-		#post.author = form.author.data
-		post.slug = form.slug.data
-		post.content = form.content.data
-		# Update Database
-		db.session.add(post)
-		db.session.commit()
-		flash("Post Has Been Updated!")
-		return redirect(url_for('post', id=post.id))
-	
-	if current_user.id == post.poster_id or current_user.id == 14:
-		form.title.data = post.title
-		#form.author.data = post.author
-		form.slug.data = post.slug
-		form.content.data = post.content
-		return render_template('edit_post.html', form=form)
-	else:
-		flash("You Aren't Authorized To Edit This Post...")
-		posts = Posts.query.order_by(Posts.date_posted)
-		return render_template("posts.html", posts=posts)
-
-
-
-# Add Post Page
-@app.route('/add-post', methods=['GET', 'POST'])
-#@login_required
-def add_post():
-	form = PostForm()
-
-	if form.validate_on_submit():
-		poster = current_user.id
-		post = Posts(title=form.title.data, content=form.content.data, poster_id=poster, slug=form.slug.data)
-		# Clear The Form
-		form.title.data = ''
-		form.content.data = ''
-		#form.author.data = ''
-		form.slug.data = ''
-
-		# Add post data to database
-		db.session.add(post)
-		db.session.commit()
-
-		# Return a Message
-		flash("Blog Post Submitted Successfully!")
-
-	# Redirect to the webpage
-	return render_template("add_post.html", form=form)
-
-
-
-# Json Thing
-@app.route('/date')
-def get_current_date():
-	favorite_pizza = {
-		"John": "Pepperoni",
-		"Mary": "Cheese",
-		"Tim": "Mushroom"
-	}
-	return favorite_pizza
-	#return {"Date": date.today()}
 
 
 
@@ -310,16 +184,13 @@ def delete(id):
 		flash("Sorry, you can't delete that user! ")
 		return redirect(url_for('dashboard'))
 
-# Update Database Record
+# Update User
 @app.route('/update/<int:id>', methods=['GET', 'POST'])
 @login_required
 def update(id):
 	form = UserForm()
 	name_to_update = Users.query.get_or_404(id)
 	if request.method == "POST":
-		name_to_update.name = request.form['name']
-		name_to_update.email = request.form['email']
-		name_to_update.favorite_color = request.form['favorite_color']
 		name_to_update.username = request.form['username']
 		try:
 			db.session.commit()
@@ -341,36 +212,22 @@ def update(id):
 
 
 
-#def index():
-#	return "<h1>Hello World!</h1>"
-
-# FILTERS!!!
-#safe
-#capitalize
-#lower
-#upper
-#title
-#trim
-#striptags
-
 
 @app.route('/user/add', methods=['GET', 'POST'])
 def add_user():
 	name = None
 	form = UserForm()
 	if form.validate_on_submit():
-		user = Users.query.filter_by(email=form.email.data).first()
+		user = Users.query.filter_by(username=form.username.data).first()
 		if user is None:
 			# Hash the password!!!
 			hashed_pw = generate_password_hash(form.password_hash.data, "sha256")
-			user = Users(username=form.username.data, name=form.name.data, email=form.email.data, favorite_color=form.favorite_color.data, password_hash=hashed_pw)
+			user = Users(username=form.username.data, password_hash=hashed_pw)
 			db.session.add(user)
 			db.session.commit()
 		name = form.name.data
 		form.name.data = ''
 		form.username.data = ''
-		form.email.data = ''
-		form.favorite_color.data = ''
 		form.password_hash.data = ''
 
 		flash("User Added Successfully!")
@@ -383,22 +240,7 @@ def add_user():
 # Create a route decorator
 @app.route('/')
 def index():
-	first_name = "John"
-	stuff = "This is bold text"
-
-	favorite_pizza = ["Pepperoni", "Cheese", "Mushrooms", 41]
-	return render_template("index.html", 
-		first_name=first_name,
-		stuff=stuff,
-		favorite_pizza = favorite_pizza)
-
-# localhost:5000/user/John
-@app.route('/user/<name>')
-
-def user(name):
-	return render_template("user.html", user_name=name)
-
-# Create Custom Error Pages
+	
 
 # Invalid URL
 @app.errorhandler(404)
@@ -410,10 +252,10 @@ def page_not_found(e):
 def page_not_found(e):
 	return render_template("500.html"), 500
 
-# Create Password Test Page
+#Password Test
 @app.route('/test_pw', methods=['GET', 'POST'])
 def test_pw():
-	email = None
+	username = None
 	password = None
 	pw_to_check = None
 	passed = None
@@ -422,70 +264,37 @@ def test_pw():
 
 	# Validate Form
 	if form.validate_on_submit():
-		email = form.email.data
+		username = form.username.data
 		password = form.password_hash.data
 		# Clear the form
-		form.email.data = ''
+		form.usernmame.data = ''
 		form.password_hash.data = ''
 
 		# Lookup User By Email Address
-		pw_to_check = Users.query.filter_by(email=email).first()
+		pw_to_check = Users.query.filter_by(username=username).first()
 		
 		# Check Hashed Password
 		passed = check_password_hash(pw_to_check.password_hash, password)
 
 	return render_template("test_pw.html", 
-		email = email,
+		username = username,
 		password = password,
 		pw_to_check = pw_to_check,
 		passed = passed,
 		form = form)
 
 
-# Create Name Page
-@app.route('/name', methods=['GET', 'POST'])
-def name():
-	name = None
-	form = NamerForm()
-	# Validate Form
-	if form.validate_on_submit():
-		name = form.name.data
-		form.name.data = ''
-		flash("Form Submitted Successfully!")
-		
-	return render_template("name.html", 
-		name = name,
-		form = form)
-
-
-
-
-# Create a Blog Post model
-class Posts(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	title = db.Column(db.String(255))
-	content = db.Column(db.Text)
-	#author = db.Column(db.String(255))
-	date_posted = db.Column(db.DateTime, default=datetime.utcnow)
-	slug = db.Column(db.String(255))
-	# Foreign Key To Link Users (refer to primary key of the user)
-	poster_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+#MODELS
 
 # Create Model
 class Users(db.Model, UserMixin):
 	id = db.Column(db.Integer, primary_key=True)
 	username = db.Column(db.String(20), nullable=False, unique=True)
-	name = db.Column(db.String(200), nullable=False)
-	email = db.Column(db.String(120), nullable=False, unique=True)
-	favorite_color = db.Column(db.String(120))
-	about_author = db.Column(db.Text(), nullable=True)
 	date_added = db.Column(db.DateTime, default=datetime.utcnow)
 	profile_pic = db.Column(db.String(), nullable=True)
-
-	# Do some password stuff!
 	password_hash = db.Column(db.String(128))
 	# User Can Have Many Posts 
-	posts = db.relationship('Posts', backref='poster')
+	images = db.relationship('Images', backref='poster')
 
 
 	@property
